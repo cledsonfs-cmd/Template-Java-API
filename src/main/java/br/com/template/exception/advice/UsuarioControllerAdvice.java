e854b5c72a84da28ebaba9cbab81077938405266
Cledson Francisco Silva <70558441+cledsonfs-cmd@users.noreply.github.com>
2024-07-18 21:47:10 -0300

    fix:ajustes

--- src/main/java/br/com/template/exception/advice/UsuarioControllerAdvice.java
+++ src/main/java/br/com/template/exception/advice/UsuarioControllerAdvice.java
@@ -4,6 +4,7 @@ import br.com.template.exception.CampoObrigatorioException;
 import br.com.template.exception.SenhaException;
 import br.com.template.exception.UsuarioAutenticadoException;
 import br.com.template.exception.UsuarioNotFoundException;
+import br.com.template.model.dto.ErrorDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -17,36 +18,36 @@ public class UsuarioControllerAdvice {
     @ResponseBody
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(UsuarioNotFoundException.class)
-    public String handleModuloNotFound(UsuarioNotFoundException usuarioNotFoundException){
-        return "Usuario não encontrado!";
+    public ErrorDTO handleModuloNotFound(UsuarioNotFoundException usuarioNotFoundException){
+        return new ErrorDTO("Usuario não encontrado!");
     }
 
     @ResponseBody
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(CampoObrigatorioException.class)
-    public String handleModuloNotFound(CampoObrigatorioException campoObrigatorioException){
-        return "Campo obrigatorio não informado!";
+    public ErrorDTO handleModuloNotFound(CampoObrigatorioException campoObrigatorioException){
+        return new ErrorDTO("Campo obrigatorio não informado!");
     }
 
     @ResponseBody
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(SenhaException.class)
-    public String handleModuloNotFound(SenhaException senhaException){
-        return "A senha deve ter no minimo 6 caracteres!";
+    public ErrorDTO handleModuloNotFound(SenhaException senhaException){
+        return new ErrorDTO("A senha deve ter no minimo 6 caracteres!");
     }
 
     @ResponseBody
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(UsuarioAutenticadoException.class)
-    public String handleModuloNotFound(UsuarioAutenticadoException usuarioAutenticadoException){
-        return "Usuário não autenticado!";
+    public ErrorDTO handleModuloNotFound(UsuarioAutenticadoException usuarioAutenticadoException){
+        return new ErrorDTO("Usuário não autenticado!");
     }
 
     @ResponseBody
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(AuthenticationException.class)
-    public String handleModuloNotFound(AuthenticationException exception){
-        return exception.getMessage();
+    public ErrorDTO handleModuloNotFound(AuthenticationException exception){
+        return new ErrorDTO(exception.getMessage());
     }
 
 
